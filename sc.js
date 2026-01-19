2f3fb406806c00f7b12eb50a6c272ddd53e4c3b2
Keshav Abhishek <crystaled2003@gmail.com>
2026-01-19 13:20:25 +0530

    Added Footer

--- sc.js
+++ sc.js
@@ -36,7 +36,8 @@ window.addEventListener('load', () => {
         const loader = document.getElementById('loader');
         loader.style.opacity = '0';
         setTimeout(() => { loader.style.display = 'none'; }, 700);
-    }, 1500);
+    }, 0); // make it 1500
+
 });
 
 // 2. Navbar Scroll Effect
